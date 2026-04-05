dc1d5dd6e297fe6da297b425ec3311952536221f
quan-tasu-133 <quan133200666@gmail.com>
2026-04-05 18:17:29 +0700

    section9

--- Lab02/AimsProject/src/quan133/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/quan133/DigitalVideoDisc.java
@@ -6,4 +6,19 @@ public class DigitalVideoDisc {
     private String director;
     private int length;
     private float cost;
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLength() {
+		return length;
+	}
+	public float getCost() {
+		return cost;
+	}
 }
