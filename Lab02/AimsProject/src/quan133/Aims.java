e7f994d130a0924c4c1169c17d7598208927ad5f
quan-tasu-133 <quan133200666@gmail.com>
2026-04-05 21:42:55 +0700

    section12

--- Lab02/AimsProject/src/quan133/Aims.java
+++ Lab02/AimsProject/src/quan133/Aims.java
@@ -6,14 +6,20 @@ public class Aims {
         Cart anOrder = new Cart();
 
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-        anOrder.addDigitalVideoDisc(dvd1);
-
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
-        anOrder.addDigitalVideoDisc(dvd2);
-
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+
+        anOrder.addDigitalVideoDisc(dvd1);
+        anOrder.addDigitalVideoDisc(dvd2);
         anOrder.addDigitalVideoDisc(dvd3);
 
+        anOrder.printCart();
+        System.out.println("Total Cost is: ");
+        System.out.println(anOrder.totalCost());
+
+        anOrder.removeDigitalVideoDisc(dvd2);
+
+        System.out.println();
         anOrder.printCart();
         System.out.println("Total Cost is: ");
         System.out.println(anOrder.totalCost());
