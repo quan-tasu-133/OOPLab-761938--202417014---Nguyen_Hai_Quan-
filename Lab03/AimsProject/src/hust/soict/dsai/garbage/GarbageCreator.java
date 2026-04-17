ad246450132e6a2dd137abdc54e6d2069e46cbfc
quan-tasu-133 <quan133200666@gmail.com>
2026-04-17 17:13:20 +0700

    Section6

--- Lab03/AimsProject/src/hust/soict/dsai/garbage/GarbageCreator.java
+++ Lab03/AimsProject/src/hust/soict/dsai/garbage/GarbageCreator.java
@@ -6,7 +6,7 @@ import java.nio.file.Paths;
 public class GarbageCreator {
     public static void main(String[] args) throws Exception {
 
-        String filename = "test.txt";
+        String filename = "test.txt"; 
 
         byte[] inputBytes = Files.readAllBytes(Paths.get(filename));
 
