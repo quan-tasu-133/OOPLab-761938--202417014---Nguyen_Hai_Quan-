ea63c7ee02af257fa158d222d6253f247d730ba7
quan-tasu-133 <quan133200666@gmail.com>
2026-04-21 18:05:16 +0700

    Section 14

--- Lab03/AimsProject/src/hust/soict/dsai/aims/store/Store.java
+++ Lab03/AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -1,34 +1,22 @@
 package hust.soict.dsai.aims.store;
 
-import hust.soict.dsai.aims.DigitalVideoDisc;
+import java.util.ArrayList;
+import hust.soict.dsai.aims.media.Media;
 
 public class Store {
 
-    private DigitalVideoDisc[] itemsInStore = new DigitalVideoDisc[100];
-    private int qtyInStore = 0;
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
 
-    public void addDVD(DigitalVideoDisc dvd) {
-        if (qtyInStore < itemsInStore.length) {
-            itemsInStore[qtyInStore] = dvd;
-            qtyInStore++;
-            System.out.println("DVD added to store");
-        } else {
-            System.out.println("Store is full");
-        }
+    public Store() {
     }
 
-    public void removeDVD(DigitalVideoDisc dvd) {
-        for (int i = 0; i < qtyInStore; i++) {
-            if (itemsInStore[i] == dvd) {
-                for (int j = i; j < qtyInStore - 1; j++) {
-                    itemsInStore[j] = itemsInStore[j + 1];
-                }
-                itemsInStore[qtyInStore - 1] = null;
-                qtyInStore--;
-                System.out.println("DVD removed from store");
-                return;
-            }
-        }
-        System.out.println("DVD not found");
+    public void addMedia(Media media) {
+        itemsInStore.add(media);
+        System.out.println("The media has been added");
+    }
+
+    public void removeMedia(Media media) {
+        itemsInStore.remove(media);
+        System.out.println("The media has been removed");
     }
 }
--- Lab03/AimsProject/src/hust/soict/dsai/aims/store/StoreTest.java
+++ Lab03/AimsProject/src/hust/soict/dsai/aims/store/StoreTest.java
@@ -1,20 +1,16 @@
 package hust.soict.dsai.aims.store;
 
-import hust.soict.dsai.aims.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Book;
 
 public class StoreTest {
     public static void main(String[] args) {
 
         Store store = new Store();
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King");
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars");
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin");
+        Book book = new Book();
+        book.setTitle("Java");
 
-        store.addDVD(dvd1);
-        store.addDVD(dvd2);
-        store.addDVD(dvd3);
-
-        store.removeDVD(dvd2);
+        store.addMedia(book);
+        store.removeMedia(book);
     }
 }
